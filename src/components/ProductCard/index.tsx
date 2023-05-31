147d00581f3fce00d660b6c39899ef3b9a5524c1
Gustavo Lima <gustavowell098@gmail.com>
2023-05-31 09:42:24 -0300

    added skeleton loading

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { ReactComponent as ArrowUpRight } from "../../assets/arrow-up-right.svg";
 import { FamilyProps } from "./types";
 
@@ -9,34 +10,35 @@ export const ProductCard: React.FC<FamilyProps> = ({ family }) => {
     return `https://plugin-storage.nyc3.digitaloceanspaces.com/families/images/${id}.jpg`;
   };
 
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   return (
     <div className="flex h-[234px] w-44 flex-col items-center rounded-lg border-2 border-solid border-gray-100">
       <div className="flex flex-1 items-center justify-center">
-        {family === undefined ? (
-          <Skeleton height={100} width={100} />
-        ) : (
-          <img
-            className="w-36 max-h-[155px]"
-            src={getFamilyImageURL(family.id)}
-            alt="Produto"
-          />
-        )}
+        <img
+          className={`${imageLoaded ? "" : "hidden"} max-h-[155px] w-36`}
+          src={getFamilyImageURL(family.id)}
+          alt="Produto"
+          onLoad={() => setImageLoaded(true)}
+        />
+
+        <Skeleton
+          className={`${imageLoaded ? "hidden" : ""}`}
+          height={150}
+          width={120}
+        />
       </div>
-      
+
       <div className="flex h-9 w-full items-center justify-center border-t-2 border-solid border-gray-100">
-        {family === undefined ? (
-          <Skeleton height={20} width={152} />
-        ) : (
-          <div className="flex h-full w-full py-2">
-            <p className="flex h-full flex-1 items-center pl-[6px] text-productTitle font-semibold leading-3">
-              {family.details.name}
-            </p>
-            <span className="flex h-full w-[2px] rounded-full bg-gray-100" />
-            <span className="flex px-1">
-              <ArrowUpRight width={20} height={20} />
-            </span>
-          </div>
-        )}
+        <div className="flex h-full w-full py-2">
+          <p className="flex h-full flex-1 items-center pl-[6px] text-productTitle font-semibold leading-3">
+            {family.details.name}
+          </p>
+          <span className="flex h-full w-[2px] rounded-full bg-gray-100" />
+          <span className="flex px-1">
+            <ArrowUpRight width={20} height={20} />
+          </span>
+        </div>
       </div>
     </div>
   );
